b58bf8bee2a8061255072664dc3615c72aaa28de
Micah <micah.olivares@jortechnology.com>
2024-04-30 17:17:39 -0400

    Added a check for required extensions to make sure they are supported by Vulkan.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <set>
+#include <string>
 
 // GLOBALS
 const uint32_t WIDTH = 800;
@@ -20,8 +23,6 @@ public:
 	}
 
 private:
-	// Initialization, mainloop, and cleanup:
-	
 	void initWindow()
 	{
 		glfwInit();
@@ -29,7 +30,7 @@ private:
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+		mWindow = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
 
 	void initVulkan()
@@ -39,7 +40,7 @@ private:
 
 	void mainLoop()
 	{
-		while (!glfwWindowShouldClose(window))
+		while (!glfwWindowShouldClose(mWindow))
 		{
 			glfwPollEvents();
 		}
@@ -47,35 +48,46 @@ private:
 
 	void cleanup()
 	{
-		vkDestroyInstance(instance, nullptr);
+		vkDestroyInstance(mInstance, nullptr);
 
-		glfwDestroyWindow(window);
+		glfwDestroyWindow(mWindow);
 		glfwTerminate();
 	}
 
 private:
+	std::vector<VkExtensionProperties> getAvaliableExtensions()
+	{
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		std::vector<VkExtensionProperties> avaliableExtensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, avaliableExtensions.data());
+
+		return avaliableExtensions;
+	}
+
+	std::vector<const char*> getRequiredExtensions()
+	{
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		std::vector<const char*> requiredExtensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+		return requiredExtensions;
+	}
+
+	bool checkExtensionSupport(const std::vector<VkExtensionProperties>& avaliableExtensions, const std::vector<const char*>& requiredExtensions)
+	{
+		std::set<std::string> requiredExtensionsSet(requiredExtensions.begin(), requiredExtensions.end());
+		for (const auto& extension : avaliableExtensions)
+		{
+			requiredExtensionsSet.erase(extension.extensionName);
+		}
+
+		return requiredExtensionsSet.empty();
+	}
+
 	void createInstance()
 	{
-		/*
-			The Vulkan instance is the entrypoint for interacting with the graphics and compute system. Creating the instance initializes the Vulkan
-			API and allows for the querying of physical devices such as GPUs. It also enables the application to configure global settings and parameters
-			such as validation layers and extensions that affect the way Vulkan behaves at runtime. 
-
-			struct VkApplicationInfo
-			{
-				VkStructureType    sType;
-				const void*        pNext;
-				const char*        pApplicationName;
-				uint32_t           applicationVersion;
-				const char*        pEngineName;
-				uint32_t           engineVersion;
-				uint32_t           apiVersion;
-			};
-
-			The Vulkan struct VkApplicationInfo specifies information about the application to the Vulkan implementation. This contains information such as
-			the application's name, version, and engine. This is neccassary to help Vulkan understand how the application needs to behave.
-		*/
-
 		VkApplicationInfo appInfo{};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		appInfo.pApplicationName = "Hello Triangle";
@@ -84,58 +96,31 @@ private:
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.apiVersion = VK_API_VERSION_1_0;
 
-		/*
-			struct VkInstanceCreateInfo 
-			{
-				VkStructureType             sType;
-				const void*                 pNext;
-				VkInstanceCreateFlags       flags;
-				const VkApplicationInfo*    pApplicationInfo;
-				uint32_t                    enabledLayerCount;
-				const char* const*          ppEnabledLayerNames;
-				uint32_t                    enabledExtensionCount;
-				const char* const*          ppEnabledExtensionNames;
-			};
-
-			The Vulkan struct VkInstanceCreateInfo specifies the create information for the instnace. It tells the Vulkan driver which global extensions
-			and validation layers we want to use. Global here means that they apply to the entire program and not a specific device. 
-		*/
+		std::vector<VkExtensionProperties> avaliableExtensions = getAvaliableExtensions();
+		std::vector<const char*> requiredExtensions = getRequiredExtensions();
+		if (!checkExtensionSupport(avaliableExtensions, requiredExtensions))
+		{
+			throw std::runtime_error("Extensions requested, but are not supported!");
+		}
+		uint32_t requiredExtensionCount = static_cast<uint32_t>(requiredExtensions.size());
+		const char** requiredExtensionNames = requiredExtensions.data();
 
 		VkInstanceCreateInfo createInfo{};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		createInfo.pApplicationInfo = &appInfo;
-
-		uint32_t glfwExtensionCount = 0;
-		const char** glfwExtensions;
-
-		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-		createInfo.enabledExtensionCount = glfwExtensionCount;
-		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledExtensionCount = requiredExtensionCount;
+		createInfo.ppEnabledExtensionNames = requiredExtensionNames;
 		createInfo.enabledLayerCount = 0;
 
-		/*
-			VKAPI_ATTR VkResult VKAPI_CALL vkCreateInstance
-			(
-				const VkInstanceCreateInfo*                 pCreateInfo,
-				const VkAllocationCallbacks*                pAllocator,
-				VkInstance*                                 pInstance
-			);
-
-			The Vulkan function vkCreateInstance takes in a reference to the instance's create info, optional allocator, and a reference to the instance
-			handle.	If the function is successful, it returns VK_SUCCESS of type VkResult and a handle to the instance. 
-		*/
-
-		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+		if (vkCreateInstance(&createInfo, nullptr, &mInstance) != VK_SUCCESS)
 		{
 			throw std::runtime_error("Failed to create instance!");
 		}
 	}
 
-private:	
-	// GLFW and Vulkan handle objects:
-	GLFWwindow* window;
-	VkInstance instance;
+private:
+	GLFWwindow* mWindow;
+	VkInstance mInstance;
 };
 
 int main()
