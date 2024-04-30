7c2d2a1bbfdb397d3d7e28d3bc741c0fa0dfb07e
Micah <micah.olivares@jortechnology.com>
2024-04-29 23:07:33 -0400

    Added the Vulkan instance.

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,13 @@ public:
 	}
 
 private:
+	// Initialization, mainloop, and cleanup:
+	
 	void initWindow()
 	{
 		glfwInit();
 
-		// Hints to GLFW to not use the OpenGL API.
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
@@ -34,12 +34,11 @@ private:
 
 	void initVulkan()
 	{
-		// Calls other helper functions that initialize Vulkan objects.
+		createInstance();
 	}
 
 	void mainLoop()
 	{
-		// Mainloop continues to run until the GLFW window is closed or an error occurs.
 		while (!glfwWindowShouldClose(window))
 		{
 			glfwPollEvents();
@@ -48,13 +47,95 @@ private:
 
 	void cleanup()
 	{
-		// Cleans up GLFW resources.
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 		glfwTerminate();
 	}
 
 private:
+	void createInstance()
+	{
+		/*
+			The Vulkan instance is the entrypoint for interacting with the graphics and compute system. Creating the instance initializes the Vulkan
+			API and allows for the querying of physical devices such as GPUs. It also enables the application to configure global settings and parameters
+			such as validation layers and extensions that affect the way Vulkan behaves at runtime. 
+
+			struct VkApplicationInfo
+			{
+				VkStructureType    sType;
+				const void*        pNext;
+				const char*        pApplicationName;
+				uint32_t           applicationVersion;
+				const char*        pEngineName;
+				uint32_t           engineVersion;
+				uint32_t           apiVersion;
+			};
+
+			The Vulkan struct VkApplicationInfo specifies information about the application to the Vulkan implementation. This contains information such as
+			the application's name, version, and engine. This is neccassary to help Vulkan understand how the application needs to behave.
+		*/
+
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine"; 
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		/*
+			struct VkInstanceCreateInfo 
+			{
+				VkStructureType             sType;
+				const void*                 pNext;
+				VkInstanceCreateFlags       flags;
+				const VkApplicationInfo*    pApplicationInfo;
+				uint32_t                    enabledLayerCount;
+				const char* const*          ppEnabledLayerNames;
+				uint32_t                    enabledExtensionCount;
+				const char* const*          ppEnabledExtensionNames;
+			};
+
+			The Vulkan struct VkInstanceCreateInfo specifies the create information for the instnace. It tells the Vulkan driver which global extensions
+			and validation layers we want to use. Global here means that they apply to the entire program and not a specific device. 
+		*/
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		/*
+			VKAPI_ATTR VkResult VKAPI_CALL vkCreateInstance
+			(
+				const VkInstanceCreateInfo*                 pCreateInfo,
+				const VkAllocationCallbacks*                pAllocator,
+				VkInstance*                                 pInstance
+			);
+
+			The Vulkan function vkCreateInstance takes in a reference to the instance's create info, optional allocator, and a reference to the instance
+			handle.	If the function is successful, it returns VK_SUCCESS of type VkResult and a handle to the instance. 
+		*/
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+		{
+			throw std::runtime_error("Failed to create instance!");
+		}
+	}
+
+private:	
+	// GLFW and Vulkan handle objects:
 	GLFWwindow* window;
+	VkInstance instance;
 };
 
 int main()
